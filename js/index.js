987948685e6fd784ee251ac5905e8bd27b34e0e0
Stiven Dorado <golds9013@gmail.com>
2024-10-10 21:31:42 -0500

    MALDITA ZORRA

---
+++ js/index.js
@@ -0,0 +1,15 @@
+const title = document.querySelector('.title')
+const text = 'Para Jheny, Olvide decirte algo   importante'.split('')
+for (let index = 0; index < text.length; index++) {
+  if (text[index] !== ' ') {
+    title.innerHTML += `<span>${text[index]}<span/>`
+  } else {
+    title.innerHTML += `<span style='margin-right: 16px;'><span/>`
+  }
+}
+
+const textElements = document.querySelectorAll('.title span');
+textElements.forEach((element) => {
+  const randomDelay = Math.random() * 3; // Menghasilkan delay acak antara 0 hingga 3 detik
+  element.style.animationDelay = `${randomDelay}s`;
+});
